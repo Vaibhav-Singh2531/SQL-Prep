5134b2b2bf6cc41003ff5385e254051208c73a9a
Vaibhav-Singh2531 <vaibhavsingh2531@gmail.com>
2026-04-10 23:44:13 +0530

    optimezed version

--- Student Course Management System/tables.sql
+++ Student Course Management System/tables.sql
@@ -1,33 +1,29 @@
 CREATE TABLE students (
-student_id VARCHAR(6) NOT NULL,
-name VARCHAR(20),
-age INT,
-department VARCHAR(50),
-PRIMARY KEY (student_id)
-)
-
+    student_id VARCHAR(6) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    age INT,
+    department VARCHAR(50)
+);
 
 CREATE TABLE courses (
-course_id VARCHAR(6) UNIQUE NOT NULL,
-course_name VARCHAR(20) UNIQUE NOT NULL,
-credits INT NOT NULL,
-PRIMARY KEY (course_id)
-)
+    course_id VARCHAR(6) PRIMARY KEY,
+    course_name VARCHAR(50) NOT NULL,
+    credits INT NOT NULL
+);
 
 CREATE TABLE enrollments (
-enrollment_id VARCHAR(6) UNIQUE NOT NULL,
-student_id VARCHAR(6) NOT NULL,
-course_id VARCHAR(6) NOT NULL,
-enrollment_date DATE,
-PRIMARY KEY (enrollment_id),
-FOREIGN KEY (student_id) REFERENCES students(student_id),
-FOREIGN KEY (course_id) REFERENCES courses(course_id)
-)
+    enrollment_id VARCHAR(6) PRIMARY KEY,
+    student_id VARCHAR(6) NOT NULL,
+    course_id VARCHAR(6) NOT NULL,
+    enrollment_date DATE,
+    FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE,
+    FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE
+);
 
 CREATE TABLE grades (
-grade_id VARCHAR(6) UNIQUE NOT NULL,
-enrollment_id VARCHAR(6) NOT NULL,
-grade VARCHAR(1) CHECK (grade IN ('A', 'B', 'C', 'D')),
-PRIMARY KEY (grade_id),
-FOREIGN KEY (enrollment_id) REFERENCES enrollments(enrollment_id)
-)
+    grade_id VARCHAR(6) PRIMARY KEY,
+    enrollment_id VARCHAR(6) NOT NULL,
+    grade VARCHAR(2) CHECK (grade IN ('A', 'B', 'C', 'D', 'F')),
+    FOREIGN KEY (enrollment_id) REFERENCES enrollments(enrollment_id) ON DELETE CASCADE
+);
