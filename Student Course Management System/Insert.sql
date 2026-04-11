5ba60c73656a9a6308b759b983156955445a90e8
Vaibhav-Singh2531 <vaibhavsingh2531@gmail.com>
2026-04-11 21:41:12 +0530

    insert query

---
+++ Student Course Management System/Insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO students (student_id,name,age,department) VALUES 
+ ('001','ajay',14,'Computer'),
+ ('002','rachit',15,'Electrical'),
+ ('003','bheem',13,'Chemical'),
+ ('004','abdul',16,'Mechanical'),
+ ('005','mahesh',15,'Electrical');
+
+INSERT INTO courses (course_id,course_name,credits) VALUES 
+  ('00A','AIML',3),
+  ('00B','CCVT',2),
+  ('00C','DS',3),
+  ('00D','DevOps',2);
+
