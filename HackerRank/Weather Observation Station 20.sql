72c722fc7b6ebf58a9e79b73cddfcc3d1d015f09
Vaibhav-Singh2531 <vaibhavsingh2531@gmail.com>
2026-06-04 23:11:05 +0530

    Weather Observation Station 20

---
+++ HackerRank/Weather Observation Station 20.sql
@@ -0,0 +1,8 @@
+SELECT ROUND(LAT_N, 4)
+FROM (
+    SELECT LAT_N,
+           ROW_NUMBER() OVER (ORDER BY LAT_N) AS rn,
+           COUNT(*) OVER () AS total_rows
+    FROM STATION
+) t
+WHERE rn = CEILING(total_rows / 2);
